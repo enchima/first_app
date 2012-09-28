b41deb83bc62d511dda5274205bc8ce054e849c7
root <root@mail.enchima.jp>
2012-09-28 11:57:24 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f4ecac72732638dcb126846ffa01177cc994de79b5c1d648af6cf0cf25f9d424302e4eea7f985c84a9b82cb6c9e006b9a4220c4f5f4f95386e5dac14b0a40ca7'
